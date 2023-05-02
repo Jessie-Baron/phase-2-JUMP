d6b5278a4fe44ec3eb7367aa1c077075ca407d48
Jessie Baron <jessiebaron.political@gmail.com>
2023-05-02 10:07:28 -0400

    practice

---
+++ phase 2 general practice/sakilla_practice.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+db = mysql.connector.connect (
+    host = 'localhost',
+    user = 'root',
+    password = 'password',
+    database = 'sakila'
+)
+
+cursor = db.cursor()
+
+# query 1
+
+query = "SELECT customer_id, count(inventory_id) FROM rental GROUP BY customer_id ORDER BY count(inventory_id) DESC LIMIT 5"
+
+cursor.execute(query)
+results = cursor.fetchall()
+print(results)
+
+# query 2
+
+
